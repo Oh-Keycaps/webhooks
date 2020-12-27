bd3a0e3bf32d7c2b64e186387e6522fa5f83d48c
Robert Snyder <immeraufdemhund@hotmail.com>
2020-12-27 11:40:41 -0600

    add urls to information

--- ImmerDiscordBot.TrelloListener/TrelloObjects/ActionTypes.cs
+++ ImmerDiscordBot.TrelloListener/TrelloObjects/ActionTypes.cs
@@ -1,5 +1,9 @@
 ﻿namespace ImmerDiscordBot.TrelloListener.TrelloObjects
 {
+    /// <summary>
+    /// https://developer.atlassian.com/cloud/trello/guides/rest-api/action-types/
+    /// https://docs.google.com/spreadsheets/d/1opvJZ2yqfWgVr5ol1NXkvhn7Y4RKon2xPhemMbQprwA/edit#gid=0
+    /// </summary>
     public static class ActionTypes
     {
         /// <summary></summary>
